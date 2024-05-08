5bb83b55940d3154df12b7cc82d36d69fae87f78
Hamberfim <adhamlingithub@gmail.com>
2024-05-08 16:54:16 -0500

    HotReloading with 'dotnet watch'

---
+++ 04_chapt/HotReloading/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Visual Studio Code: HotReloading/Program.cs using dotnet watch
+- dotnet watch run
+
+ change the output message while running the code to see the changes without manually rebuilding the app again */
+
+while (true)
+{
+    Console.WriteLine("Goodbye, watch!");
+    Thread.Sleep(2000);  // delay for 2 seconds
+}
