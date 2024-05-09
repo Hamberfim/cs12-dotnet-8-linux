e644214e7d2da9751c6c233e899c4d4ccb54e848
Hamberfim <adhamlingithub@gmail.com>
2024-05-09 13:28:20 -0500

    logging

---
+++ 04_chapt/Instrumenting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;  // needed to use Debug and Trace classes
+
+// get the current directory
+string currentDir = Environment.CurrentDirectory;
+WriteLine($"Current working directory: {currentDir}");
+
+// config a trace listener
+string logWritePath = Path.Combine(currentDir, "log.txt");
+
+
+
+WriteLine("This is a Console.WriteLine() message");
+// messages using the two classes
+Debug.WriteLine("This is a Debug.WriteLine() message");
+Trace.WriteLine("This is a Trace.WriteLine() message");
