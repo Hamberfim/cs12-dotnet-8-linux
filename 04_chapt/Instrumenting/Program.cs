edda6c302d06a4b2672f6757177bdf384d6e8488
Hamberfim <adhamlingithub@gmail.com>
2024-05-09 15:06:54 -0500

    config trace

--- 04_chapt/Instrumenting/Program.cs
+++ 04_chapt/Instrumenting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;  // needed to use Debug and Trace classes
+using Microsoft.Extensions.Configuration; // to use ConfigurationBuilder
 
 /* config a trace listener */
 // get the current directory
@@ -29,9 +30,37 @@ WriteLine("This is a Console.WriteLine() message");
 Debug.WriteLine("This is a Debug.WriteLine() message");
 Trace.WriteLine("This is a Trace.WriteLine() message");
 
+// create a configuration builder to look for the appsettings.json file
+string settingsFile = "appsettings.json";
+string settingsPath = Path.Combine(currentDir, settingsFile);
+WriteLine($"Processing: {settingsPath}");
+WriteLine($"--{settingsFile}");
+WriteLine(File.ReadAllText(settingsPath));
+WriteLine("----");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(currentDir);
+/* add the appsettings.json file to the configuration builder, 
+making it mandatory so an exception will be thrown if the the file is not found */
+builder.AddJsonFile(settingsFile, optional: false, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build(); // build the configuration
+
+TraceSwitch traceSwitch = new(displayName: "PacktSwitch", description: "This switch is set via the appsettings.json config file");
+configuration.GetSection("PacktSwitch").Bind(traceSwitch); // bind the configuration to the trace switch
+
+WriteLine($"Trace Switch Value: {traceSwitch.Value}"); // write the trace switch value to the console
+WriteLine($"Trace Switch Level: {traceSwitch.Level}"); // write the trace switch level to the console
+
+Trace.WriteLineIf(traceSwitch.TraceError, "A Trace Error message");
+Trace.WriteLineIf(traceSwitch.TraceWarning, "A Trace Warning message");
+Trace.WriteLineIf(traceSwitch.TraceInfo, "A Trace Info message");
+Trace.WriteLineIf(traceSwitch.TraceVerbose, "A Trace Verbose message");
+
 // flush and close any buffered trace listeners for Debug and Trace
-// Debug.close();
-// Trace.close();
+// Debug.close();  // error Debug does not contain a definition for close
+// Trace.close();  // error Trace does not contain a definition for close
 logFile.Close(); // close the log file
 
 // run in the terminal: dotnet run --configuration Release
@@ -40,3 +69,6 @@ logFile.Close(); // close the log file
 /* adding NuGet packages:
 dotnet add package Microsoft.Extensions.Configuration.Binder
 dotnet add package Microsoft.Extensions.Configuration.Json
+*/
+WriteLine("Press any key to exit");
+ReadLine();
