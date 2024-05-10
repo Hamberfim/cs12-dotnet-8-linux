bfb334d25f62a734e11726840aee883cc031ba18
Hamberfim <adhamlingithub@gmail.com>
2024-05-10 13:55:21 -0500

    write tests

--- 04_chapt/CalculatorLibUnitTests/UnitTest1.cs
+++ 04_chapt/CalculatorLibUnitTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using CalculatorLib;
+
 namespace CalculatorLibUnitTests;
 
 public class CalculatorUnitTests
@@ -5,40 +7,64 @@ public class CalculatorUnitTests
     [Fact]
     public void TestAdding2And2()
     {
-        // arrange
+        // arrange - set up inputs
+        double num1 = 2;
+        double num2 = 2;
+        double expected = 4;
+        Calculator calculator = new Calculator();
 
-        // act
+        // act - execute the function
+        double actual = calculator.Add(num1, num2);
 
-        // assert
+        // assert - compare the expected and actual results
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void TestSubtracting2And2()
     {
-        // arrange
+        // arrange - set up inputs
+        double num1 = 2;
+        double num2 = 2;
+        double expected = 0;
+        Calculator calculator = new Calculator();
 
-        // act
+        // act - execute the function
+        double actual = calculator.Subtract(num1, num2);
 
-        // assert
+        // assert - compare the expected and actual results
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void TestMultiplying2And2()
     {
-        // arrange
+        // arrange - set up inputs
+        double num1 = 2;
+        double num2 = 2;
+        double expected = 4;
+        Calculator calculator = new Calculator();
 
-        // act
+        // act - execute the function
+        double actual = calculator.Multiply(num1, num2);
 
-        // assert
+        // assert - compare the expected and actual results
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void TestDividing2And2()
     {
-        // arrange
+        // arrange - set up inputs
+        double num1 = 2;
+        double num2 = 2;
+        double expected = 1;
+        Calculator calculator = new Calculator();
 
-        // act
+        // act - execute the function
+        double actual = calculator.Division(num1, num2);
 
-        // assert
+        // assert - compare the expected and actual results
+        Assert.Equal(expected, actual);
     }
 }
