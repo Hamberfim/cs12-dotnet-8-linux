af9f532f70c77ca769fedfa8308a0d7c0e61ced8
Hamberfim <adhamlingithub@gmail.com>
2024-05-10 13:33:40 -0500

    calculator library

--- 04_chapt/CalculatorLib/Calculator.cs
+++ 04_chapt/CalculatorLib/Calculator.cs
@@ -1,6 +1,24 @@
 ﻿namespace CalculatorLib;
 
-public class Class1
+public class Calculator
 {
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
 
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Division(double num1, double num2)
+    {
+        return num1 / num2; // This will throw an exception if num2 is 0
+    }
 }
