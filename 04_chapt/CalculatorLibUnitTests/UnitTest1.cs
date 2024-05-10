5795d0b37ea18ba0096ef81dfa9bbc7366b85589
Hamberfim <adhamlingithub@gmail.com>
2024-05-10 13:41:55 -0500

    calculator library unit tests

--- 04_chapt/CalculatorLibUnitTests/UnitTest1.cs
+++ 04_chapt/CalculatorLibUnitTests/UnitTest1.cs
@@ -11,4 +11,34 @@ public class CalculatorUnitTests
 
         // assert
     }
+
+    [Fact]
+    public void TestSubtracting2And2()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    [Fact]
+    public void TestMultiplying2And2()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
+
+    [Fact]
+    public void TestDividing2And2()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
 }
