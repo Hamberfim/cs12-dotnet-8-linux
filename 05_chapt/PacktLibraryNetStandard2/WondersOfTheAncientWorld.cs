0400be2fded28d807caa26c16ba71f98d362e874
Hamberfim <adhamlingithub@gmail.com>
2024-05-11 12:11:33 -0500

    comment code

--- 05_chapt/PacktLibraryNetStandard2/WondersOfTheAncientWorld.cs
+++ 05_chapt/PacktLibraryNetStandard2/WondersOfTheAncientWorld.cs
@@ -3,6 +3,7 @@ using Packt.Shared;
 [Flags]  // this decoration allows for a combination of values to be stored in a single variable or field
 public enum WondersOfTheAncientWorld : byte
 {
+    // explicit byte values for each enum so they don't overlap
     None = 0b_0000_0000, // i.e., 0
     GreatPyramidOfGiza = 0b_0000_0001, // i.e., 1
     HangingGardensOfBabylon = 0b_0000_0010, // i.e., 2
